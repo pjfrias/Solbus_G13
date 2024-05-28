dae0c2394072a8005e015bcdc093bc950446c0a8
pjfrias <pjfrias@gmail.com>
2024-05-28 16:25:01 -0300

    creacion proyecto

---
+++ SolBusG13/src/Main/JavaApplication92.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Main;
+
+public class JavaApplication92 {
+
+    public static void main(String[] args) {
+     
+    }
+
+}
